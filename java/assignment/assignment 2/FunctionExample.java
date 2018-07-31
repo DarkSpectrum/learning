3316e7c4645c42d817a2c482f885242bdd78ef58
Fraser <fraserteched@gmail.com>
2018-07-31 20:42:34 +0800

    Assignment 2 Draft

---
+++ java/assignment/assignment 2/FunctionExample.java
@@ -0,0 +1,11 @@
+class FunctionExample {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 6;
+        PrintFunction(a, b);
+    }
+
+public static void PrintFunction(int a, int b) {
+        System.out.println();
+    }
+}
